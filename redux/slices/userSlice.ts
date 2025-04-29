5b33aa9e5a1e1ad2eb50772da5c1ef5f05b4fde3
Cumali Yelbiz <cumaliyelbiz2003@gmail.com>
2025-04-29 12:01:20 +0300

    son

--- redux/slices/userSlice.ts
+++ redux/slices/userSlice.ts
@@ -43,7 +43,7 @@ export const loginUserAsync = createAsyncThunk(
   async (userData: { email: string; password: string }, { rejectWithValue }) => {
     try {
       // API çağrısı
-      const response = await fetch('http://localhost:3001/api/users/login', {
+      const response = await fetch('https://api.tv42.com.tr/api/users/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
